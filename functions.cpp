9eb7d7ec29ca62f9a248bfa3df8657529b3532bf
Olena Matrunych <47101257+matrunych@users.noreply.github.com>
2020-06-13 15:57:33 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 //Main function for testing all methods
-double eggholder_function [10](x, y) {
+double eggholder_function(double x, double y) {
 //    minimum: f(512,404.2319)=-959.6407
 //    -512 < x,y < 512
     return -(y + 47) * sin(sqrt(abs(x / 2 + (y + 47))))
@@ -10,7 +10,7 @@ double eggholder_function [10](x, y) {
 }
 
 
-double ackley_function(x, y) {
+double ackley_function(double x, double y) {
 //    minimum: f(0,0)=0
 //    -5 <= x,y <= 5
     return (-20 * exp(-0.2 * pow(0.5 * (pow(x, 2) + pow(y, 2)), 0.5))
@@ -18,14 +18,14 @@ double ackley_function(x, y) {
 }
 
 
-double booth_function(x, y) {
+double booth_function(double x, double y) {
     //    f(x,y)=(x+2y-7)^2+(2x+y-5)^{2} : f(1,3)=0
     //    -10 <= x,y <= 10
     return pow(x + 2 * y - 7, 2) + pow(2 * x + y - 5, 2);
 }
 
 
-double matyas_function(x, y) {
+double matyas_function(double x, double y) {
 //    f(x,y)=0.26(x^2+y^2)-0.48xy
 //    minium: f(0,0)=0
 //    -10 <= x,y <= 10
@@ -33,20 +33,20 @@ double matyas_function(x, y) {
 }
 
 
-double rosenbrock_function (x, y) {
+double rosenbrock_function (double x, double y) {
 //   f(1.0,1.0)=0
 //   -1.5 <= x, y <= 1.5
     return pow(1 - x, 2) + 100 * pow(y - pow(x, 2), 2);
 }
 
 
-double f2d_1(x) {
+double f2d_1(double x) {
 //    2.7 <= x <= 7.5
     return sin(x) + sin(3.33 * x);
 }
 
 
-double f2d_2(x) {
+double f2d_2(double x) {
 //    minimum f (0.9669) = -1.48907
 //    0 <= x <= 1.2
     return -(1.4 - 3 * x) * sin(18 * x);
